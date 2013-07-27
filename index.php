2dd0c7df8d673073d416a25f1948729c6c32404b
Ubuntu <ubuntu@ip-172-31-22-249.us-west-2.compute.internal>
2013-07-27 11:51:47 +0000

    * implemented shell executing of python script and returing json

--- index.php
+++ index.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * create image from base64 encoding
+ *
+ */
+
+define(EXTENSION,'.png');
+$prefix = 'temp_';
+$filename = '';
 
 if(isset($_POST['base64'])) {
 	$data = $_POST['base64']; 
@@ -11,16 +19,23 @@ $data = 'iVBORw0KGgoAAAANSUhEUgAAABwAAAASCAMAAAB/2U7WAAAABl'
        . '8a5HSE35Q3eO2XP1A1wQkZSgETvDtKdQAAAABJRU5ErkJggg==';
 }
 
+
 $data = base64_decode($data);
+
 
 $im = imagecreatefromstring($data);
 if ($im !== false) {
-    header('Content-Type: image/png');
-    header('Content-Length: '. strlen($data));
-    imagepng($im);
-    imagedestroy($im);
-    //TODO execute command 
-    // change this to return JSON
+    $count = 0;
+    $filename = $prefix  . $count . EXTENSION;
+     while (file_exists($filename)) {
+	    $count++;
+	    $filename = $prefix  . $count . EXTENSION;
+    }
+    $status = imagepng($im, $filename);
+    $output = shell_exec("python imageproc.py " . $filename); 
+    unlink($filename);
+    header('Content-Type: application/json');
+    echo $output;
 }
 else {
     echo 'An error occurred.';
